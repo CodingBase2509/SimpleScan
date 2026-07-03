3968c187162a7d782c63bef43fa21f8dbdea5247
fabian <famabiker@gmail.com>
2026-07-03 18:09:47 +0200

    add basic home page layout with mock data

--- SimpleScan/Components/Pages/Home/Features/DeviceList.razor.cs
+++ SimpleScan/Components/Pages/Home/Features/DeviceList.razor.cs
@@ -1,5 +1,16 @@
+using Microsoft.AspNetCore.Components;
+using SimpleScan.Domain.Scanners;
+
 namespace SimpleScan.Components.Pages.Home.Features;
 
 public partial class DeviceList
 {
+    [Parameter]
+    public List<ScannerDevice> Devices { get; set; } = [];
+    
+    [Parameter]
+    public ScannerDevice? SelectedDevice { get; set; }
+    
+    [Parameter]
+    public EventCallback<ScannerDevice> SelectedDeviceChanged { get; set; }
 }
